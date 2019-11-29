71dbc55c2d9817280285c74250db024124a8716e
Asad Irfan <gsoftconsulting01@gmail.com>
2019-11-29 21:00:49 +0500

    add user to store

--- src/app/dashbored/dashbored.component.ts
+++ src/app/dashbored/dashbored.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit , OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { ProfileService } from '../services/userProfile.service';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState, selectAuthenticationState } from './../store/app.states';
+import { UserProfile } from './../store/actions/auth.actions';
 
 @Component({
   selector: 'app-dashbored',
@@ -9,13 +13,26 @@ import { ProfileService } from '../services/userProfile.service';
 })
 export class DashboredComponent implements OnInit , OnDestroy {
   currentUser: any;
+  getState: Observable<any>;
 
-  constructor(public router: Router, private userDataService: ProfileService) { }
+  constructor(public router: Router,
+    private userDataService: ProfileService,
+    private store: Store<AppState>,
+    ) {
+      this.getState = this.store.select(selectAuthenticationState);
+     }
 
   ngOnInit(): void {
-    this.userDataService.getUserData().subscribe(res => {
-      this.currentUser = res.result;
-      console.log('user:', res.result);
+    // this.userDataService.getUserData().subscribe(res => {
+    //   this.currentUser = res.result;
+    //   console.log('user:', res.result);
+    // });
+    this.getState.subscribe((state) => {
+      this.currentUser = state.userProfile;
+      console.log( 'dash' ,  this.currentUser);
+      if (!this.currentUser) {
+        this.store.dispatch(new UserProfile());
+      }
     });
 
     const body = document.getElementsByTagName('body')[0];
