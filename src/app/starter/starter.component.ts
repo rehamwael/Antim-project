8c07b36c98229cd48e0825a34aef488dacc67768
rehamwael <rehamhabbas@hotmail.com>
2019-12-23 19:54:34 +0200

    fix

--- src/app/starter/starter.component.ts
+++ src/app/starter/starter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-starter',
@@ -10,14 +11,23 @@ export class StarterComponent implements OnInit {
 
   userLang: any;
   
-    constructor( public translate: TranslateService) {
+    constructor( public router: Router , public translate: TranslateService) {
         this.translate.onLangChange.subscribe((event) => {
           this.userLang=event.lang;
           console.log(this.userLang); 
         });
     }
 
-  ngOnInit() {
+    ngOnInit(): void {
+      this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
   }
-
+  ngAfterViewInit() {}
+  btnClick= function () {
+    this.router.navigateByUrl('/signup');
+  };
 }
