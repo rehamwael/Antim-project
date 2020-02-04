c0f73e62f7234cd3595e873632522ca8be1a451a
Asad Irfan <gsoftconsulting01@gmail.com>
2020-02-04 18:13:07 +0500

    add static data in t&c page

--- src/app/terms-conditions/terms-conditions.component.ts
+++ src/app/terms-conditions/terms-conditions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ProfileService } from '../services/userProfile.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-terms-conditions',
@@ -8,10 +10,31 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TermsConditionsComponent implements OnInit , OnDestroy {
   userLang: any;
+  TermsConditions: any = {
+    page_name: 'Terms And Conditions Page',
+    Section: {
+      section_name: 'Terms And Conditions',
+      TitleEn: '',
+      TitleAr: '',
+      ContentEn: '',
+      ContentAr: '',
+      ParagraphEn: '',
+      ParagraphAr: '',
+      EnBulletPoints: [{
+        bulletPoint: ''
+      }],
+      ArBulletPoints:  [{
+        bulletPoint: ''
+      }],
+    },
+  };
 
   constructor(
     public translate: TranslateService,
+    private profileService: ProfileService,
+    private spinner: NgxSpinnerService,
   ) {
+    this.userLang = this.translate.currentLang;
     this.translate.onLangChange.subscribe((event) => {
       this.userLang = event.lang;
     });
@@ -22,12 +45,21 @@ export class TermsConditionsComponent implements OnInit , OnDestroy {
     const body = document.getElementsByTagName('body')[0];
     body.classList.add('contact');
     body.classList.add('who-we-are');
+
+    this.spinner.show();
+    this.profileService.getStaticPageByKey('TermsAndConditionsPage').subscribe(res => {
+      // console.log(res);
+      this.spinner.hide();
+      this.TermsConditions = JSON.parse(res.result.sections);
+    }, err => {
+      this.spinner.hide();
+      console.log(err);
+    });
   }
   ngOnDestroy(): void {
     const body = document.getElementsByTagName('body')[0];
     body.classList.remove('contact');
     body.classList.remove('who-we-are');
-
   }
 
 }
